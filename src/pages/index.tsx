1a6272c6a40fb05bc98491c76ee49e117e09588c
Chris <ahnkhit@gmail.com>
2025-02-28 15:29:13 +0100

    début d'un header calma je viens de commencer momo je sais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex m-3">
-      <p className="font-medium text-white">RetroBox</p>
-      <div className="border border-white rounded-full text-white ml-1">
-        <p className="ml-2 mr-2">Retrobox</p>
-      </div>
+    <div>
+      <header className="w-full h-20 flex justify-between items-center bg-blue-500 px-4">
+        <div className="text-white font-bold">Rétrobox</div>
+        <nav className="flex gap-[5%] ml-[10%]">
+          <a href="#" className="text-white border border-white px-2 py-1 px-[20px] rounded-full">Jeux</a>
+          <a href="#" className="text-white border border-white px-2 py-1 px-[20px] rounded-full">Console</a>
+          <a href="#" className="text-white border border-white px-2 py-1 px-[20px] rounded-full">Actualités</a>
+          <a href="#" className="text-white border border-white px-2 py-1 px-[20px] rounded-full">Boutique</a>
+        </nav>
+        <div className="text-white border border-white px-2 py-1 px-[20px] rounded-full ml-auto">Connexion</div>
+      </header>
     </div>
   );
 }
-
-
-{/* <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Bienvenue sur ma page Next.js</h1>
-        <p className="text-lg text-gray-700 mb-6">Ceci est une page de base avec du style.</p>
-        <button className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition">
-          Cliquez-moi
-        </button>
-      </div> */}
