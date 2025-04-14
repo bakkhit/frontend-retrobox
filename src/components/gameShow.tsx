96dc1f72812878318370fbc4856337525020c922
Chris <ahnkhit@gmail.com>
2025-04-14 16:12:41 +0200

    game show size fixed

--- src/components/gameShow.tsx
+++ src/components/gameShow.tsx
@@ -21,15 +21,15 @@ const GameShow = ({ data }: Props) => {
 
     return (
         <section
-            className="w-[100vw] h-[100%] bg-cover bg-center bg-no-repeat p-10 transition-all duration-500 bg-black"
+            className="relative w-[100vw] h-[50vw] bg-cover bg-no-repeat bg-center p-10 transition-all duration-500 bg-black"
             style={{ backgroundImage: `url(${selectedGame.img})` }}
         >
-            <div className="text-white w-[25%] m-[13vw] text-[1vw]">
+            <div className="text-white w-[25%] mt-[13vw] ml-[13vw] text-[1vw] z-10">
                 <h2 className="text-[2vw] font-bold mb-2">{selectedGame.name}</h2>
                 <p className="mb-4">{selectedGame.description}</p>
             </div>
 
-            <div className="flex flex-wrap justify-center gap-4 mb-6">
+            <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex justify-center gap-4 z-10">
                 {data.slice(0, 6).map((game) => (
                     <img
                         key={game.id}
@@ -37,13 +37,14 @@ const GameShow = ({ data }: Props) => {
                         alt={game.name}
                         onClick={() => setSelectedGame(game)}
                         className={`w-[10vw] object-cover rounded-xl cursor-pointer transition-transform duration-300 border-4 ${selectedGame.id === game.id
-                            ? "border-white scale-105"
-                            : "border-transparent hover:scale-105"
+                                ? "border-white scale-105"
+                                : "border-transparent hover:scale-105"
                             }`}
                     />
                 ))}
             </div>
         </section>
+
     );
 };
 
